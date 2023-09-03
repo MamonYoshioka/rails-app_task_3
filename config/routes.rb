dd21026e6f05320435b5372144245f8593cdb523
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-09-03 06:04:08 +0000

    [add] いいね機能とコメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
 
-
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create]
+    resources :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:index,:show,:edit,:update]
 
 end
